d67cc15f8a2edc498768fbd4911a0a8fc7040fac
IndrayaniW <38439215+IndrayaniW@users.noreply.github.com>
2023-03-10 16:30:50 +0100

    added subheaders

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner - Changes made')
  
+streamlit.subheader('Omega 3 & Blueberry Oatmeal')
+ 
+streamlit.subheader('Kale, Spinach & Rocket Smoothie')
+ 
+streamlit.subheader('Hard-Boiled Free-Range Egg')
