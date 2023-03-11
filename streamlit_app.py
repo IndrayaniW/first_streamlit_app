9314531a1c53c41559be6f210668a0d5b3d2d7dd
IndrayaniW <38439215+IndrayaniW@users.noreply.github.com>
2023-03-11 18:21:05 +0100

    Update streamlit_app.py
    changed streamlit header to be before the response variable of the fruityvice api

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # 11.3.23 new section to display fruityvice api response. Shows <Response [200]> in the streamlit-created web-app
+streamlit.header('Fruityvice Fruit Advice!') # positioning makes a difference in the display
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
-streamlit.header('Fruityvice Fruit Advice!')
+
