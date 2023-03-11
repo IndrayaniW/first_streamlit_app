2209612b501896b7f832f0a4cff368b2a9591852
IndrayaniW <38439215+IndrayaniW@users.noreply.github.com>
2023-03-11 20:58:07 +0100

    Update streamlit_app.py
    querying actual data - changing test lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,9 @@ streamlit.dataframe(fruityvice_normalized)
 ## querying data from Snowflake - after having connected SF to Streamlit
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")# for testing only
+my_cur.execute("select * from fruit_load_list")# querying actual data from SF
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+#streamlit.text("Hello from Snowflake:")# for testing only
+streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
