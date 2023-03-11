a44bbaeae3a52537eb77712d48ec439a5e515c24
IndrayaniW <38439215+IndrayaniW@users.noreply.github.com>
2023-03-11 21:50:07 +0100

    Update streamlit_app.py
    🥋 Move the Fruit Load List Query and Load into a Button Action

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,18 +95,20 @@ except URLError as e:
 streamlit.stop()
 
 
-## querying data from Snowflake - after having connected SF to Streamlit
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")# for testing only
-my_cur.execute("select * from fruit_load_list")# querying actual data from SF
-#my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
-#streamlit.text("Hello from Snowflake:")# for testing only
-streamlit.header("The fruit load list contains:")
-#streamlit.text(my_data_row)# shows json format
-streamlit.dataframe(my_data_rows)# shows table / dataframe format
 
+streamlit.header("The fruit load list contains:")
+
+# adding Snowflake related functions
+def get_fruit_load_list():
+ with my_cnx.cursor() as my_cur:
+  my_cur.execute("select * from fruit_load_list")# querying actual data from SF
+  return my_cur.fetchall()
+ 
+# add a button to lad the fruit
+if streamlit.button('Get Fruit Load List'):
+ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+ my_data_rows = get_fruit_load_list()
+ streamlit.dataframe(my_data_rows)# shows table / dataframe format
 
 # adding new section as entry box to allow adding fruits
 fruit_added = streamlit.text_input('What fruit would you like to add?') # using text input
