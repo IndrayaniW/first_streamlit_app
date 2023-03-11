f1dcf25f65a92434c5e4fd8873a1d852f6763e1e
IndrayaniW <38439215+IndrayaniW@users.noreply.github.com>
2023-03-11 18:19:31 +0100

    Update streamlit_app.py
    added streamlit header

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display the selected fruits only
 streamlit.dataframe(fruits_to_show)
 
-# 11.3.23 new section to display fruityvice api response
+# 11.3.23 new section to display fruityvice api response. Shows <Response [200]> in the streamlit-created web-app
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
+streamlit.header('Fruityvice Fruit Advice!')
